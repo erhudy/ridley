3f23dc246a33ceb245fa595e189f519d4e1b1b70
Edmund Rhudy <erhudy@users.noreply.github.com>
2025-10-04 12:13:49 -0400

    Add debug flag

--- constants.go
+++ constants.go
@@ -5,6 +5,7 @@ import "time"
 var (
 	FLAG_DEFAULT_CHANNEL_LENGTH = 100
 	FLAG_DEFAULT_CLIENT_TIMEOUT = 10 * time.Second
+	FLAG_DEFAULT_DEBUG          = false
 	FLAG_DEFAULT_LISTEN_ADDRESS = "0.0.0.0:8080"
 	FLAG_DEFAULT_SWITCH_TIMEOUT = 30 * time.Second
 	FLAG_DEFAULT_TARGET         = "http://localhost:9090/api/v1/write"
@@ -12,6 +13,7 @@ var (
 
 	FLAG_NAME_CHANNEL_LENGTH = "channel-length"
 	FLAG_NAME_CLIENT_TIMEOUT = "client-timeout"
+	FLAG_NAME_DEBUG          = "debug"
 	FLAG_NAME_LISTEN_ADDRESS = "listen-address"
 	FLAG_NAME_SWITCH_TIMEOUT = "switch-timeout"
 	FLAG_NAME_TARGET         = "target"
--- main.go
+++ main.go
@@ -53,19 +53,20 @@ func init() {
 	v.SetConfigType("yaml")
 	v.AddConfigPath(".")
 
-	flag.Int(FLAG_NAME_CHANNEL_LENGTH, FLAG_DEFAULT_CHANNEL_LENGTH, "TKTK")
-	flag.Duration(FLAG_NAME_CLIENT_TIMEOUT, FLAG_DEFAULT_CLIENT_TIMEOUT, "TKTK")
-	flag.String(FLAG_NAME_LISTEN_ADDRESS, FLAG_DEFAULT_LISTEN_ADDRESS, "TKTK")
-	flag.Duration(FLAG_NAME_SWITCH_TIMEOUT, FLAG_DEFAULT_SWITCH_TIMEOUT, "TKTK")
-	flag.String(FLAG_NAME_TARGET, FLAG_DEFAULT_TARGET, "TKTK")
-	flag.String(FLAG_NAME_TARGET_HEADERS, FLAG_DEFAULT_TARGET_HEADERS, "TKTK")
+	flag.Int(FLAG_NAME_CHANNEL_LENGTH, FLAG_DEFAULT_CHANNEL_LENGTH, "Set the length of the internal buffered channels")
+	flag.Duration(FLAG_NAME_CLIENT_TIMEOUT, FLAG_DEFAULT_CLIENT_TIMEOUT, "Set the timeout of the HTTP client that sends to the target")
+	flag.Bool(FLAG_NAME_DEBUG, FLAG_DEFAULT_DEBUG, "Log in debug mode")
+	flag.String(FLAG_NAME_LISTEN_ADDRESS, FLAG_DEFAULT_LISTEN_ADDRESS, "Set listen address for Ridley")
+	flag.Duration(FLAG_NAME_SWITCH_TIMEOUT, FLAG_DEFAULT_SWITCH_TIMEOUT, "Set timeout after which Ridley will switch to a different stream")
+	flag.String(FLAG_NAME_TARGET, FLAG_DEFAULT_TARGET, "Set address of target to send to")
+	flag.String(FLAG_NAME_TARGET_HEADERS, FLAG_DEFAULT_TARGET_HEADERS, "Set headers to add to requests being sent to the target in the form \"key1=val1,key2=val2\"")
 
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
 	pflag.Parse()
 	v.BindPFlags(pflag.CommandLine)
 
 	var err error
-	logger, err = zap.NewDevelopment()
+	logger, err = zap.NewProduction()
 	if err != nil {
 		panic(err)
 	}
@@ -82,6 +83,14 @@ func init() {
 		}
 	}
 
+	// replace the logger here now that we've gone through all config options and can tell for certain if we should be in debug mode
+	if v.GetBool(FLAG_NAME_DEBUG) {
+		logger, err = zap.NewDevelopment()
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	targetHeadersString := v.GetString(FLAG_NAME_TARGET_HEADERS)
 	targetHeadersMap := make(map[string]string)
 	afterSplit := strings.Split(targetHeadersString, ",")
@@ -94,9 +103,9 @@ func init() {
 		if len(entrySplit) != 2 {
 			panic(fmt.Errorf("unable to process target headers entry: %v", entrySplit))
 		}
-		k := strings.TrimSpace(strings.TrimRight(strings.TrimLeft(strings.TrimSpace(entrySplit[0]), "'\""), "'\""))
-		v := strings.TrimSpace(strings.TrimRight(strings.TrimLeft(strings.TrimSpace(entrySplit[1]), "'\""), "'\""))
-		targetHeadersMap[k] = v
+		key := strings.TrimSpace(strings.TrimRight(strings.TrimLeft(strings.TrimSpace(entrySplit[0]), "'\""), "'\""))
+		value := strings.TrimSpace(strings.TrimRight(strings.TrimLeft(strings.TrimSpace(entrySplit[1]), "'\""), "'\""))
+		targetHeadersMap[key] = value
 	}
 	v.Set(FLAG_NAME_TARGET_HEADERS, targetHeadersMap)
 
