23a5b3806c6a2f5c76d4394cef9e025e8c984bbc
CreatureKing <julesskrill@gmail.com>
2025-11-17 11:26:15 -0500

    Fix: Channels begone

--- go.mod
+++ go.mod
@@ -11,7 +11,6 @@ require (
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/prometheus/client_golang v1.23.2 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.66.1 // indirect
 	github.com/prometheus/procfs v0.16.1 // indirect
@@ -19,9 +18,6 @@ require (
 	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
 	github.com/spf13/afero v1.15.0 // indirect
 	github.com/spf13/cast v1.10.0 // indirect
-	github.com/spf13/pflag v1.0.10 // indirect
-	github.com/spf13/viper v1.21.0 // indirect
-	github.com/stretchr/testify v1.11.1 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.yaml.in/yaml/v2 v2.4.2 // indirect
@@ -31,4 +27,9 @@ require (
 	google.golang.org/protobuf v1.36.8 // indirect
 )
 
-require go.uber.org/zap v1.27.0
+require (
+	github.com/prometheus/client_golang v1.23.2
+	github.com/spf13/pflag v1.0.10
+	github.com/spf13/viper v1.21.0
+	go.uber.org/zap v1.27.0
+)
--- remote_write_handler.go
+++ remote_write_handler.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"net/http"
-	"sync"
 	"time"
-
-	"go.uber.org/zap"
 )
+
+type RemoteWriteHandler struct {
+	client      *http.Client
+	connTracker *ConnTracker
+}
 
 func (rwh *RemoteWriteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	now := time.Now()
-	logger.Debug("remote addr", zap.String("ip:port", r.RemoteAddr))
+	logger.Debugw("remote addr", "ip:port", r.RemoteAddr)
 	/* example headers:
 	{
 		"Content-Encoding":["snappy"],
@@ -26,116 +27,59 @@ func (rwh *RemoteWriteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request)
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "error processing request: %s", err.Error())
+		logger.Errorw("error processing request", "error", err.Error())
 		metric_FailedIncomingRequestsTotal.WithLabelValues().Inc()
 		return
 	}
-	if r.Header.Get(HEADER_X_RIDLEY_REPLICA) == "" {
+	replica := r.Header.Get(HEADER_X_RIDLEY_REPLICA)
+	if replica == "" {
 		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "%s header must be set on every request", HEADER_X_RIDLEY_REPLICA)
+		logger.Errorw("missing required header", "header", HEADER_X_RIDLEY_REPLICA)
 		metric_FailedIncomingRequestsTotal.WithLabelValues().Inc()
 		return
 	}
-	req := RequestWithTimestamp{
-		requestBody:    body,
-		requestHeaders: r.Header.Clone(),
-		timestamp:      now,
-	}
 
-	rwh.requestChan <- req
-	w.WriteHeader(http.StatusAccepted)
+	rwh.HandleRequest(w, r.Header, body, now, replica)
 }
 
-func (rwh *RemoteWriteHandler) Dispatch() {
-	logger.Debug("starting dispatch")
-	wg := sync.WaitGroup{}
-	go rwh.sendToTarget()
-	stopping := false
-	for {
-		select {
-		case request := <-rwh.requestChan:
-			replica := request.requestHeaders.Get(HEADER_X_RIDLEY_REPLICA)
-			// for some reason during shutdown we get a request with no replica header set so if replica is "" just skip
-			if replica != "" {
-				requestQueue, exists := rwh.connTracker.GetOrCreate(replica)
-				if !exists {
-					logger.Info("spawning new process replica", zap.String("replica", replica))
-					wg.Go(func() { rwh.processReplica(requestQueue, replica) })
-				}
-				requestQueue <- request
-			}
-			if stopping {
-				rwh.connTracker.Shutdown()
-				wg.Wait()
-				close(rwh.sendChan)
-				return
-			}
-		case <-rwh.quitChan:
-			stopping = true
-		}
-	}
-}
+func (rwh *RemoteWriteHandler) HandleRequest(w http.ResponseWriter, headers http.Header, body []byte, recievedTime time.Time, replica string) {
+	if rwh.connTracker.TrySetActiveConnection(replica, recievedTime) {
+		metric_IncomingRequestsTotal.WithLabelValues(replica).Inc()
+		logger.Debugw("handling request", "replica", replica)
 
-func (rwh *RemoteWriteHandler) processReplica(requestQueue chan RequestWithTimestamp, replica string) {
-	switchTimeout := v.GetDuration(FLAG_NAME_SWITCH_TIMEOUT)
-	for {
-		select {
-		case request := <-requestQueue:
-			logger.Debug("processReplica handling request", zap.String("replica", replica))
-
-			lrqTs := rwh.connTracker.GetActiveLastRequestTimestamp()
-			if lrqTs == nil {
-				logger.Info("first request received for replica", zap.String("replica", replica))
-			} else {
-				logger.Debug("time since last request forwarded", zap.Duration("duration", time.Since(*lrqTs)))
-				if time.Since(*lrqTs) > switchTimeout {
-					logger.Warn("switching active replica due to timeout of previous active", zap.Duration("timeoutDuration", switchTimeout))
-					rwh.connTracker.SetActiveConnection(replica)
-					logger.Warn("new active replica", zap.String("replica", replica))
-				}
-			}
-
-			metric_IncomingRequestsTotal.WithLabelValues(replica).Inc()
-			if rwh.connTracker.IsReplicaActive(replica) {
-				logger.Debug("forwarding request for active replica", zap.String("replica", replica))
-				rwh.connTracker.SetActiveLastRequestTimestamp(time.Now())
-				rwh.sendChan <- request
-			} else {
-				logger.Debug("discarding request", zap.String("replica", replica))
-			}
-		case <-rwh.quitChan:
-			logger.Info("stopping process replica", zap.String("replica", replica))
+		target := v.GetString(FLAG_NAME_TARGET)
+		addlHeaders := v.GetStringMapString(FLAG_NAME_TARGET_HEADERS)
+		req, err := http.NewRequest(http.MethodPost, target, bytes.NewReader(body))
+		if err != nil {
+			logger.Errorw("failed to create HTTP request", "error", err.Error())
+			metric_SendErrorsTotal.WithLabelValues("599").Inc()
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-	}
 
-}
-
-func (rwh *RemoteWriteHandler) sendToTarget() {
-	target := v.GetString(FLAG_NAME_TARGET)
-	addlHeaders := v.GetStringMapString(FLAG_NAME_TARGET_HEADERS)
-	for request := range rwh.sendChan {
-		req, err := http.NewRequest(http.MethodPost, target, bytes.NewReader(request.requestBody))
-		if err != nil {
-			logger.Error("failed to create HTTP request", zap.Error(err))
-			metric_SendErrorsTotal.WithLabelValues("599").Inc()
-			continue
-		}
-		req.Header = request.requestHeaders
+		req.Header = headers.Clone()
 		for headerKey, headerValue := range addlHeaders {
 			req.Header.Set(headerKey, headerValue)
 		}
+
 		resp, err := rwh.client.Do(req)
 		if err != nil {
-			logger.Error("failed to send HTTP request", zap.Error(err))
+			logger.Errorw("failed to send HTTP request", "error", err)
 			metric_SendErrorsTotal.WithLabelValues("599").Inc()
-			continue
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
-		if resp.StatusCode > 299 {
-			logger.Error("got unexpected error code", zap.Int("status", resp.StatusCode))
-			metric_SendErrorsTotal.WithLabelValues(fmt.Sprintf("%d", resp.StatusCode)).Inc()
-			continue
+
+		defer resp.Body.Close()
+		respBody, err := io.ReadAll(resp.Body)
+		if err != nil {
+			logger.Errorw("failed to read response body", "error", err)
+		} else {
+			w.Write(respBody)
 		}
-		resp.Body.Close()
+		w.WriteHeader(resp.StatusCode)
+	} else {
+		logger.Debugw("discarding request", "replica", replica)
+		w.WriteHeader(http.StatusAccepted)
 	}
 }
